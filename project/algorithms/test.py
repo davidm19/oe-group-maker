732124893616f41321e3411b0e7c75b9812e55d9
D Malone <dmf@Ds-MacBook-Air.local>
2018-10-17 13:55:14 -0700

    Adapt algorithm from men and women to two groups (really it's just changing names)

--- project/algorithms/test.py
+++ project/algorithms/test.py
@@ -1,45 +1,38 @@
 from match import Matcher
 
-
-# the men and their list of ordered spousal preferences
-M = dict((m, prefs.split(', ')) for [m, prefs] in (line.rstrip().split(': ')
+G1 = dict((g1m, prefs.split(', ')) for [g1m, prefs] in (line.rstrip().split(': ')
                                 for line in open('men.txt')))
 
-# the women and their list of ordered spousal preferences
-W = dict((m, prefs.split(', ')) for [m, prefs] in (line.rstrip().split(': ')
+G2 = dict((g2m, prefs.split(', ')) for [g2m, prefs] in (line.rstrip().split(': ')
                                 for line in open('women.txt')))
 
+match = Matcher(G1, G2)
 
-# initialize Matcher with preference lists for both men and women
-match = Matcher(M, W)
-
-# check if the mapping of wives to husbands is stable
-def is_stable(wives, verbose=False):
-    for w, m in wives.items():
-        i = M[m].index(w)
-        preferred = M[m][:i]
+def is_stable(g2groups, verbose=False):
+    for g2m, g1m in g2groups.items():
+        i = G1[g1m].index(g2m)
+        preferred = G1[g1m][:i]
         for p in preferred:
-            h = wives[p]
-            if W[p].index(m) < W[p].index(h):  
+            h = g2groups[p]
+            if G2[p].index(g1m) < G2[p].index(h):
                 msg = "{}'s marriage to {} is unstable: " + \
                       "{} prefers {} over {} and {} prefers " + \
                       "{} over her current husband {}"
                 if verbose:
-                    print msg.format(m, w, m, p, w, p, m, h) 
+                    print msg.format(g1m, g2m, g1m, p, g2m, p, g1m, h)
                 return False
     return True
 
-# match men and women; returns a mapping of wives to husbands
-wives = match()
-assert is_stable(wives)             # should be a stable matching
+g2groups = match()
+assert is_stable(g2groups)             # should be a stable matching
 assert match.is_stable()            # should be a stable matching
 
-# swap the husbands of two wives, which should make the matching unstable
-wives['fay'], wives['gay'] = wives['gay'], wives['fay']
+# swap the husbands of two g2groups, which should make the matching unstable
+g2groups['fay'], g2groups['gay'] = g2groups['gay'], g2groups['fay']
 
-assert is_stable(wives) is False    # should not be a stable matching
+assert is_stable(g2groups) is False    # should not be a stable matching
 
-# with the perturbed matching we find that gav's marriage to fay is unstable: 
+# with the perturbed matching we find that gav's marriage to fay is unstable:
 #
-#   * gav prefers gay over fay 
+#   * gav prefers gay over fay
 #   * gay prefers gav over her current husband dan
