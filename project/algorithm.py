4554d068697f36874ffd7189935f18ee86bce0f5
D Malone <dmf@Ds-MacBook-Air.local>
2018-10-19 08:26:18 -0700

    Import requisite modules for algorithm file

--- project/algorithm.py
+++ project/algorithm.py
@@ -2,6 +2,7 @@ import flask
 from flask import Flask, request
 from server.dao import Customer, engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Student, PreferredMember
 
 def getStudents():
     students = query.all()
