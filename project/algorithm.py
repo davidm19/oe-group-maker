1b237fda4ac1e3f502340836461df216867e6ee9
D Malone <dmf@Ds-MacBook-Air.local>
2018-10-19 08:19:33 -0700

    Add algorithm.py

--- project/algorithm.py
+++ project/algorithm.py
@@ -3,5 +3,6 @@ from flask import Flask, request
 from server.dao import Customer, engine
 from sqlalchemy.orm import sessionmaker
 
-def getCharacters():
-    customer
+def getStudents():
+    students = query.all()
+    return students
