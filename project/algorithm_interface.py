ff9b5d53aeaaffe9d5b344a96329c684762d571a
D Malone <dmf@Ds-MacBook-Air.local>
2018-11-06 09:01:30 -0800

    Start from the beginning (delete unnecessary algorithm files)

---
+++ project/algorithm_interface.py
@@ -0,0 +1,15 @@
+#THIS IS WHERE YOU IMPORT STUFF
+
+'''
+
+ALGORITHM INTERFACE
+1. select trip from database
+2. algorithm step 1
+3. algorithm step 2
+4. algorithm step 3
+5. export list in some format
+
+'''
+
+def select_trip():
+    
---
+++ project/algorithm_testcases.py
@@ -0,0 +1,12 @@
+'''
+
+TESTCASES:
+- test if we can successfully select trip
+- test if we can pass in a trip that doesn't exist
+- test if we can select students at various points in a list/or pass in a student that doesn't exist
+- test preferences at various points/or test preferences that don't exist
+- check if algorithm steps work independently of each other
+- check if algorithm works as a whole
+
+'''
+
