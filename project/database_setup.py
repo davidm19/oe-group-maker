873712d88ceca29118e01813a6eaedbdc2938ea5
Michael Huang <mhuang2020@chadwickschool.org>
2018-11-07 08:07:30 -0800

    Feat: Add Trip to Database Setup

--- project/database_setup.py
+++ project/database_setup.py
@@ -7,12 +7,19 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
+class Trip(Base):
+    __tablename__ = 'trip'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32))
+
 class Student(Base):
     __tablename__ = 'student'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(32))
     last_name = Column(String(32))
+    trip_id = Column(Integer, ForeignKey('trip.id'))
+    trip = relationship(Trip)
 
 
 class Preference(Base):
--- project/test_database_setup.py
+++ project/test_database_setup.py
@@ -7,12 +7,19 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
+class Trip(Base):
+    __tablename__ = 'trip'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32))
+
 class Student(Base):
     __tablename__ = 'student'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(32))
     last_name = Column(String(32))
+    trip_id = Column(Integer, ForeignKey('trip.id'))
+    trip = relationship(Trip)
 
 
 class Preference(Base):
@@ -23,6 +30,7 @@ class Preference(Base):
     priority = Column(Integer)
     student_id = Column(Integer, ForeignKey('student.id'))
     student = relationship(Student)
+
 
 engine = create_engine('sqlite:///test_database.db')
 Base.metadata.create_all(engine)
