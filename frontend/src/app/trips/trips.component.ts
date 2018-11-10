6b0d650b14349e2e0bfd753f41532d50ab4be1c6
miadimson2019 <miadimson2019@chadwickschool.org>
2018-11-09 20:04:15 -0800

    added trips.component.ts

---
+++ frontend/src/app/trips/trips.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {Trip} from './trip.model';
+import {TripsApiService} from './trips-api.service';
+
+@Component({
+  selector: 'trips',
+  template: `
+    <div>
+      <button routerLink="/new-trip">New trip</button>
+      <ul>
+        <li *ngFor="let trip of tripsList">
+          {{trip.title}}
+        </li>
+      </ul>
+    </div>
+  `
+})
+export class TripsComponent implements OnInit, OnDestroy {
+  tripsListSubs: Subscription;
+  tripsList: Trip[];
+
+  constructor(private tripsApi: TripsApiService) {
+  }
+
+  ngOnInit() {
+    this.tripsListSubs = this.tripsApi
+      .getTrips()
+      .subscribe(res => {
+          this.tripsList = res;
+        },
+        console.error
+      );
+  }
+
+  ngOnDestroy() {
+    this.tripsListSubs.unsubscribe();
+  }
+}
