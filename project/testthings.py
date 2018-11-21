7143b57f84ae883379aa5d887af00d7a15c920de
Michael Huang <mhuang2020@chadwickschool.org>
2018-11-21 12:04:04 -0800

    Test: Add foundation of test case for step 2: Does not print results yet

--- project/testthings.py
+++ project/testthings.py
@@ -18,3 +18,16 @@ session = DBSession()
 
 
 class BasicTests(unittest.TestCase):
+
+    def test_stepTwo(self):
+        newStudent(Char, Lie, Paul, Sam, Kel, session)
+        newStudent(Pete, R, Kel, Sam, Paul, session)
+        newStudent(Eli, S, Sam, Kel, Paul, session)
+        newStudent(Paul, Ly, Eli, Char, Sam, session)
+        newStudent(Kel, Ly, Pete, Char, Sam, session)
+        newStudent(Sam, My, Char, Paul, Eli)
+        students = session.query(Student).all()
+        for student in students:
+            remove_lowpriority_pairs(student, session)
+        expected_results = {{Char, Lie, Paul, Sam}, {Pete, R, Kel},{Eli,S, Sam, Paul}, {Paul,Ly, Eli, Char}, {Kel,Ly, Pete}, {Sam, My, Char, Eli}}
+        results = {}
