40d9c9fde44476d05b8095ed417622a3291c16f6
davidm19 <dmalone@chadwickschool.org>
2018-12-05 08:59:43 -0800

    feat: implement grade levels for students, begin integrating trips and students

--- project/application.py
+++ project/application.py
@@ -9,12 +9,12 @@ import random, string
 import json
 from flask import make_response
 from sqlalchemy.sql import exists
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 # CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Outdoor Ed Group Maker"
@@ -84,41 +84,56 @@ def deleteTrip(trip_id):
 
 """ ======== STUDENT CRUD METHODS ======== """
 
-@app.route('/students', methods=['GET'])
-def showStudents():
+@app.route('/trips/<int:trip_id>/students', methods=['GET'])
+def showStudents(trip_id):
     session = DBSession()
-    students = session.query(Student).all()
+    students = session.query(Student).filter_by(id=trip_id).all()
     students_all = list()
     for student in students:
         student_info = { "first_name" : student.first_name
                     , "last_name" : student.last_name
+                    , "grade" : student.grade
                     }
         students_all.append(student_info)
     return flask.jsonify(students_all), 200
 
-@app.route('/students/<int:ID>/', methods=['GET'])
-def showStudent(ID):
+@app.route('/trips/<int:trip_id>/students/<int:ID>/', methods=['GET'])
+def showStudent(ID, GRADE):
     session = DBSession()
     student = session.query(Student).filter_by(id=ID).one()
     student_info = { "first_name" : student.first_name
                 , "last_name" : student.last_name
+                , "grade" : student.grade
                 }
     return flask.jsonify(student_info), 200
 
+@app.route('/trips/<int:trip_id>/students/grades/<int:GRADE>', methods=['GET'])
+def showStudentByGradeLevel(trip_id, GRADE):
+    session = DBSession()
+    students = session.query(Student).filter_by(grade=GRADE).all()
+    students_all = list()
+    for student in students:
+        student_info = { "first_name" : student.first_name
+                    , "last_name" : student.last_name
+                    , "grade" : student.grade
+                    }
+        students_all.append(student_info)
+    return flask.jsonify(students_all), 200
+
 #NEED SHOW STUDENT + PREFS METHODS NOW!!
 
-@app.route('/students/new', methods=['POST'])
-def newStudent():
+@app.route('/trips/<int:trip_id>/students/new', methods=['POST'])
+def newStudent(trip_id):
     session = DBSession()
     post = request.get_json()
     if request.method == 'POST':
-        newStudent = Student(first_name = post["first_name"], last_name = post["last_name"])
+        newStudent = Student(first_name = post["first_name"], last_name = post["last_name"], grade = post["grade"])
     session.add(newStudent)
     session.commit()
     return flask.jsonify("Student successfully added! \n"), 200
 
-@app.route('/students/<int:id>/edit', methods=['PUT'])
-def editStudent(id):
+@app.route('/trips/<int:trip_id>/students/<int:id>/edit', methods=['PUT'])
+def editStudent(trip_id, id):
     session = DBSession()
     post = request.get_json()
     if "id" not in post:
@@ -129,12 +144,14 @@ def editStudent(id):
         editedStudent.first_name = post['first_name']
     elif 'last_name' in post:
         editedStudent.last_name = post['last_name']
+    elif 'grade' in post:
+        editedStudent.grade = post['grade']
     session.add(editedStudent)
     session.commit()
     return flask.jsonify("Student successfully updated! \n"), 200
 
-@app.route('/students/<int:id>/delete', methods=['PUT'])
-def deleteStudent(id):
+@app.route('/trips/<int:trip_id>/students/<int:id>/delete', methods=['PUT'])
+def deleteStudent(trip_id, id):
     session = DBSession()
     post = request.get_json()
     if "id" not in post:
--- project/database_setup.py
+++ project/database_setup.py
@@ -11,6 +11,7 @@ class Trip(Base):
     __tablename__ = 'trip'
     id = Column(Integer, primary_key=True, autoincrement=True)
     trip_name = Column(String(32))
+    students = relationship('Trip', secondary='student_trip_link')
 
 class Student(Base):
     __tablename__ = 'student'
@@ -18,9 +19,22 @@ class Student(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(32))
     last_name = Column(String(32))
-    # grade = Column(Integer, nullable = True)
+    grade = Column(Integer, nullable = False)
+    trip = relationship('Student', secondary='student_trip_link')
     # trip_id = Column(Integer, ForeignKey('trip.id'), nullable = True)
 
+    @property
+    def serialize(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'grade': self.grade
+        }
+
+class student_trip_link(Base):
+    __tablename__ = 'student_trip_link'
+    student_id = Column(Integer, ForeignKey('student.id') primary_key = True)
+    trip_id = Column(Integer, ForeignKey('trip.id'), primary_key = True)
 
 class Preference(Base):
     __tablename__ = 'preference'
