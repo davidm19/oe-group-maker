6394305a0f0383a1abeb93202e7fb8f49dc5ca11
davidm19 <dmalone@chadwickschool.org>
2018-12-23 10:39:44 -0800

    refactor: remove unneccesary method declaration for script

--- project/scripts/test.sh
+++ project/scripts/test.sh
@@ -13,16 +13,12 @@ EOF
 
 # SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-function test {
-	echo -e "*** SETTING UP TEST DATABASE ***" 
-	python database_setup.py --test 
-	echo -e "\n*** POPULATING DATABASE ***"
-	python database_populator.py --test
-	echo -e "\n*** TESTING PROGRAMS ***"
-	python test_app.py
-	echo -e "\n*** DELETING DATABASE ***"
-	rm test.db
-	echo -e "\n*** TESTING COMPLETED ***"
-}
-
-test
+echo -e "*** SETTING UP TEST DATABASE ***" 
+python database_setup.py --test 
+echo -e "\n*** POPULATING DATABASE ***"
+python database_populator.py --test
+echo -e "\n*** TESTING PROGRAMS ***"
+python test_app.py
+echo -e "\n*** DELETING DATABASE ***"
+rm test.db
+echo -e "\n*** TESTING COMPLETED ***"
