d22294113646ecf52d15df117afdb56497919a29
davidm19 <dmalone@chadwickschool.org>
2018-12-23 10:39:25 -0800

    feat: implement backend script

--- project/scripts/backend.sh
+++ project/scripts/backend.sh
@@ -2,3 +2,14 @@
 # - CREATE A (TEST?) DATABASE
 # - POPULATE IT
 # - PYTHON APPLICATION.PY
+
+if [[ -e test.db ]]; then
+	rm test.db
+fi
+
+echo -e "*** SETTING UP TEST DATABASE ***" 
+python database_setup.py --test 
+echo -e "\n*** POPULATING DATABASE ***"
+python database_populator.py --test
+echo -e "\n*** RUNNING SERVER ***"
+python application.py
