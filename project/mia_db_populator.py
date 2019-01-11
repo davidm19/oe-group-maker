77763c5b3fbb52f9dfb9bcec9a1e6ac06b4b16d6
miadimson2019 <miadimson2019@chadwickschool.org>
2019-01-10 23:07:13 -0800

    Created mia_db_populator.py

---
+++ project/mia_db_populator.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Trip, Student, engine, Preference
+
+engine = create_engine('sqlite:///database.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+JTree = Trip(trip_name="JTree", trip_grade='7')
+session.add(JTree)
+session.commit()
+
+John = Student(first_name='John', last_name='Doe', grade='7', trip=JTree)
+session.add(John)
+session.commit()
+
+Jane = Student(first_name='Jane', last_name='Doe', grade='7', trip=JTree)
+session.add(Jane)
+session.commit()
