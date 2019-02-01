70f094d9a6715b9908fba455d09283b9110edd52
Dylan <dnguyen2020@chadwickschool.org>
2019-02-01 11:43:35 -0800

    Improved the add trip test but it still doesn't work.

--- frontend/e2e/spec.js
+++ frontend/e2e/spec.js
@@ -1,27 +1,31 @@
 describe('OE new trip function', function(){
     beforeEach(function(){
-        browser.get('localhost:4200/');
+        browser.get('http://localhost:4200/');
     })
 
-    // var newTrip = element.all(by.css('mat-button-ripple mat-ripple mat-button-ripple-round'));
-    // var EC = protractor.ExpectedConditions;
-    // var tripName = element(by.css('mat-input-element mat-form-field-autofill-control cdk-text-field-autofill-monitored'));
-    // var tripGrade = element(by.css('mat-form-field-infix'));
-
+    var newTrip = element.all(by.css('button'));
+    var EC = protractor.ExpectedConditions;
+    var tripAdder = element.all(by.css('i'));
+    var tripName = element.all(by.css('input'));
+    var tripGrade = element(by.css('input'));
+    var saveTrip = element(by.css('button')).filter(function());
 
     it('should have a title', function(){
-        browser.getCurrentUrl('localhost:4200/');
-        expect(browser.getCurrentUrl()).toBe('localhost:4200/');
+        browser.get('http://localhost:4200/');
+        expect(browser.getCurrentUrl()).toBe('http://localhost:4200/');
     })
-    // it('should show new trip functions', function(){
-    //     newTrip.click();
-    //     broswer.wait(EC.urlContains('/trips/new'), 5000);
-    //     // expect(EC.presenceOf('tripGrade'));
-    // })
-    // it('should add a new trip', function(){
-    //     tripName.sendKeys('Test Trip');
-    //     tripGrade.sendKeys('7');
-    //     browser.wait(EC.urlContains('/trips'), 5000);
-    //     expect(element.all(by.css('mat-elevation-z5 mat-card')).getText()).toContain('Test Trip');
-    // })
+    it('should show new trip functions', function(){
+        newTrip.click();
+        expect(browser.getCurrentUrl()).toBe('http://localhost:4200/trips');
+        // expect(EC.presenceOf('tripGrade'));
+    })
+    it('should add a new trip', function(){
+        newTrip.click();
+        tripAdder.click();
+        tripName.sendKeys('Test Trip');
+        tripGrade.sendKeys('7');
+        saveTrip.click();
+        browser.wait(EC.urlContains('/trips'), 5000);
+        expect(element.all(by.css('mat-card')).getText()).toContain('Test Trip');
+    })
 });
