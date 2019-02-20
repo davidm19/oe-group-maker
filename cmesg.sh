90e005bb45ab669dd79ddb76884ab1d46e2b0c64
davidm19 <dmalone@chadwickschool.org>
2019-02-20 08:53:05 -0800

    docs: begin documenting code

--- cmesg.sh
+++ cmesg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# FOR TESTING ONLY! Make a test file holding all commits. If the test file is already present at execution, delete it
 FILE=a.txt
 
 if [[ -e $FILE ]]; then
     rm $FILE
 fi
 
+# Retrieve all commits and their authors
 git log --pretty=format:"%an: %s" > $FILE
 
+# Show file
 less $FILE
