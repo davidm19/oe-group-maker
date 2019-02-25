c6a6393191f2b9ee90cc3f57657247747f61e065
davidm19 <dmalone@chadwickschool.org>
2019-02-25 09:55:12 -0800

    refactor: make changes to code styling/formatting in both commit scripts

--- cmesg.sh
+++ cmesg.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# ========================================================================
-# FOR TESTING ONLY! Make a test file holding all commits. If the test file is already present at execution, delete it
-FILE1=a.txt
-FILE2=b.txt
+# Create a hidden commits file; if it exists at script runtime, remove it
+FILE1=.commits.txt
 
 if [[ -e $FILE1 ]]; then
     rm $FILE1
 fi
-# ========================================================================
 
-# Retrieve all commits and their authors
-# git log --pretty=format:"%an: %s" > $FILE1
+# Retrieve all commits (messages only; still need to work how authors get)
 git log --pretty=format:"%s" > $FILE1
-# git log --pretty=format:"%s" | head -n 1 > $FILE1
 
-# INPUT FILE1 TO COMMIT MSG. SCRIPT (USE THE WITH OPEN AS (VAR) THINGY) AND EXPORT THAT TO FILE2 (b.txt)
+# Run commit message python script
 python mesg_check.py
-
-# FOR TESTING ONLY! Show file
-# less $FILE1
