98b5b5bebed3d21beb1e79a26b090896facdf98c
davidm19 <dmalone@chadwickschool.org>
2019-02-26 10:52:50 -0800

    refactor: fix more errors

--- mesg_check.py
+++ mesg_check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys
+import os
 import re
 from subprocess import check_output
 from collections import defaultdict
@@ -21,11 +22,17 @@ with open('.commits.txt', 'r') as f:
         message = current_line[1]
         if current_line != "":
             if author != "J.D. DeVaughn-Brown":
-                if message.startswith("feat: ") or message.startswith("fix: ")
-                or message.startswith("refactor: ") or message.startswith("style: ")
-                or message.startswith("docs: ") or message.startswith("test: ")
-                or message.startswith("chore: ") or message.startswith("Merge ")
+                if message.startswith("feat: ")
+                or message.startswith("fix: ")
+                or message.startswith("refactor: ")
+                or message.startswith("style: ")
+                or message.startswith("docs: ")
+                or message.startswith("test: ")
+                or message.startswith("chore: ")
+                or message.startswith("Merge ")
                 or message.startswith("Initial "):
-                    print("Success! Line '%s' starts with a valid commit type." % message)
+                    print("Success! Line '%s' starts with a valid commit type."
+                            % message)
                 else:
-                    print("'%s' is invalid as it does not start with a commit type." % message)
+                    print("'%s' is invalid as it does not start with a commit type."
+                            % message)
